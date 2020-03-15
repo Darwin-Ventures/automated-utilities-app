992d9fc08a930a8e0f26371b685af67ef0876f04
Inkafoo <inkafoo@gmail.com>
2020-03-15 20:43:56 +0100

    designed login and registration view [Delivers #171801965]

--- app/src/main/java/com/example/automatedutilitiesapp/views/login_and_registration/LoginFragment.kt
+++ app/src/main/java/com/example/automatedutilitiesapp/views/login_and_registration/LoginFragment.kt
@@ -1,11 +1,20 @@
 package com.example.automatedutilitiesapp.views.login_and_registration
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.automatedutilitiesapp.R
 import com.example.automatedutilitiesapp.databinding.FragmentLoginBinding
+import com.jakewharton.rxbinding2.view.RxView
+import kotlinx.android.synthetic.main.fragment_login.*
+import java.util.concurrent.TimeUnit
 
 
 class LoginFragment : Fragment() {
@@ -18,8 +27,38 @@ class LoginFragment : Fragment() {
     ): View? {
         binding = FragmentLoginBinding.inflate(inflater)
 
+        navigateToRegistrationFragment(binding.notMemberButton)
+
         // Inflate the layout for this fragment
         return binding.root
+    }
+
+    /**
+     * Navigate to registerFragment
+     */
+    private fun navigateToRegistrationFragment(view: View){
+        RxView.clicks(view).map {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }.throttleFirst(1000, TimeUnit.MILLISECONDS).subscribe()
+    }
+
+    /**
+     * Set color of part notMemberButton text
+     */
+    private fun setNotMemberButtonTextColor() {
+        val spannable = SpannableString(getString(R.string.not_a_member_sign_up_now))
+        spannable.setSpan(
+            ForegroundColorSpan(Color.WHITE),
+            0,
+            14,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        notMemberButton.text = spannable
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setNotMemberButtonTextColor()
     }
 
 }
